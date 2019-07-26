1311302c999ea500fc48be036eca40a384dbc933
Toan Hoang <minhtoandut96@gmail.com>
2019-07-26 15:49:08 +0700

    Initialize project

---
+++ functions/index.js
@@ -0,0 +1,45 @@
+// index.js
+const admin = require('firebase-admin')
+const functions = require('firebase-functions')
+const express = require('express')
+
+admin.initializeApp({
+  credential: admin.credential.cert('./coders-tokyo-app-firebase.json'),
+  databaseURL: 'https://coders-tokyo-app.firebaseio.com',
+})
+
+const { ApolloServer, gql } = require('apollo-server-express')
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Hotdog {
+    isKosher: Boolean
+    location: String
+    name: String
+    style: String
+    website: String
+  }
+  type Query {
+    hotdogs: [Hotdog]
+  }
+`
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hotdogs: () =>
+      admin
+        .database()
+        .ref('hotdogs')
+        .once('value')
+        .then(snap => snap.val())
+        .then(val => Object.keys(val).map(key => val[key])),
+  },
+}
+
+// setup express cloud function
+const app = express()
+const server = new ApolloServer({ typeDefs, resolvers })
+server.applyMiddleware({ app, path: '/', cors: true })
+
+exports.graphql = functions.https.onRequest(app)
